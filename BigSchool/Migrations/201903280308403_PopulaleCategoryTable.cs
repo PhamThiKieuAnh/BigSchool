30fe2b659e1eb18fbad4354922ea3b5a86a5e4d6
PhamThiKieuAnh <kieuanh11091998@gmail.com>
2019-03-28 10:17:45 +0700

    a

---
+++ BigSchool/Migrations/201903280308403_PopulaleCategoryTable.cs
@@ -0,0 +1,18 @@
+namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulaleCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+            //thaydoi
+        }
+    }
+}
